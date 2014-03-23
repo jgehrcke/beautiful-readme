34f259d6e6e76e65c08d4ec2c7917bf221539da0
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-03-23 21:00:12 +0100

    Remove conf.py.sample, add brconf.py.

---
+++ brconf.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+#converter = "docutils"
+converter = "markdown"
+
+
+# Document content.
+# =================
+title = "beautiful-readme"
+description = "Creates a simple mobile-friendly static website from your README."
+about = """
+Beautiful-readme is
+<a href="http://opensource.org/licenses/MIT">MIT licensed</a>. I created it
+rather quickly for fulfilling my own needs. Nevertheless, you might want to
+give it a try, too!
+"""
+
+copyright = """© 2014 <a href="http://gehrcke.de">Jan-Philip Gehrcke</a>"""
+sidebar = """
+<div class="sidebar-module sidebar-module-inset">
+    <h4>Contribute!</h4>
+    <p>For bug reports and suggestions, please use the
+    <a href="#">issue section</a> on GitHub.
+    Pull requests are welcome.</p>
+</div>
+<div class="sidebar-module">
+<h4>Resources</h4>
+<ol class="list-unstyled">
+  <li><a href="https://github.com/jgehrcke/beautiful-readme">GitHub</a></li>
+  <li><a href="https://pypi.python.org/pypi/beautiful-readme">PyPI</a></li>
+</ol>
+</div>"""
+
+
+# Document layout.
+# ================
+# Additional CSS code.
+customcss = ""
+
+
+# Miscellaneous.
+# ==============
+# Google Analytics integration.
+# Leave string empty if you do not want the GA snippet to be included.
+google_analytics_id = ""
+
+# Build directory (relative from current working directory).
+builddir = "_build"
