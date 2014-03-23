1406d6cc4811a2c56c83a7eb631d524e8e3a7716
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-03-23 21:04:05 +0100

    Add main.py.

---
+++ beautifulreadme/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2014 Jan-Philip Gehrcke (http://gehrcke.de).
+# See LICENSE file for details.
+
+
+from .beautifulreadme import main
+#from .beautifulreadme import __version__
+
+
+if __name__ == "__main__":
+    main()
