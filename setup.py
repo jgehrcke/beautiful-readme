cf4c893560bede792e8f1c809d54a89f1c9b9870
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-03-23 23:03:02 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2014 Jan-Philip Gehrcke (http://gehrcke.de).
+# See LICENSE file for details.
+
+
+import re
+from setuptools import setup
+
+
+long_description = \
+"""Beautiful-readme converts a single README file into a simple and modern
+`Bootstrap`_-powered static website.
+
+Resources:
+- Documentation_
+- GitHub_
+
+.. _Bootstrap: http://getbootstrap.com/
+.. _GitHub: http://github.com/jgehrcke/beautiful-readme
+.. _Documentation: http://gehrcke.de/beautiful-readme
+"""
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('beautifulreadme/beautifulreadme.py').read(),
+    re.M
+    ).group(1)
+
+
+setup(
+    name = "beautiful-readme",
+    packages = ["beautifulreadme"],
+    entry_points = {
+        "console_scripts": ['beautiful-readme = beautifulreadme.main:main']
+        },
+    version = version,
+    description = "Creates a simple mobile-friendly static website from your README.",
+    author = "Jan-Philip Gehrcke",
+    author_email = "jgehrcke@googlemail.com",
+    url = "http://gehrcke.de/beautiful-readme",
+    long_description=open("README.rst", "rb").read().decode('utf-8'),
+    keywords = ["readme", "website", "static", "Bootstrap"],
+    platforms = ["POSIX", "Windows"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        ],
+    )
