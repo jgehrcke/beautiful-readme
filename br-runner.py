f691f007ef38ac43d5270801979c7bddc05b5428
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-03-23 21:00:50 +0100

    Add br-runner.py.

---
+++ br-runner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2014 Jan-Philip Gehrcke (http://gehrcke.de).
+# See LICENSE file for details.
+
+
+"""
+Convenience wrapper for testing development version of beautiful readme.
+"""
+
+
+from beautifulreadme.main import main
+
+
+if __name__ == '__main__':
+    main()
